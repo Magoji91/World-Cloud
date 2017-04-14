a572e5bdf6d3ba4fc027478078c99ed6dbf373b2
Magoji91 <smsanda2004@gmail.com>
2017-04-14 13:24:35 -0300

    Update Global.R

--- Global.R
+++ Global.R
@@ -1,8 +1,10 @@
-library(tm)
-library(wordcloud)
-library(memoise)
-library(shiny)
-library(stringr)
+
+library(tm) # for text mining
+library(wordcloud) #word-cloud generator
+library(memoise) #Cache the results of a function so that when you call it again with the same arguments it returns the pre-computed value
+library(shiny) #build interactive web applications with Rstringr
+library(stringr) #A consistent, simple and easy to use set of wrappers
+
 
 phrases <- list(Seuss = c("Do","not","cry","because","it","is","over","smile","because","it","happened"),
                 Wilde = c("Be","yourself","everyone","else", "is", "already", "taken”))
