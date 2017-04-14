d9c2981f32c4efc06e4853a2322a926692de6dbe
Magoji91 <smsanda2004@gmail.com>
2017-04-13 22:56:09 -0300

    Update Server.R

--- Server.R
+++ Server.R
@@ -1,34 +1,24 @@
-# Text of the books downloaded from:
-# A Mid Summer Night's Dream:
-#  http://www.gutenberg.org/cache/epub/2242/pg2242.txt
-# The Merchant of Venice:
-#  http://www.gutenberg.org/cache/epub/2243/pg2243.txt
-# Romeo and Juliet:
-#  http://www.gutenberg.org/cache/epub/1112/pg1112.txt
-# http://www.sthda.com/english/wiki/text-mining-and-word-cloud-fundamentals-in-r-5-simple-steps-you-should-know
-
-
-function(input, output, session) {
-  # Define a reactive expression for the document term matrix
-  terms <- reactive({
-    # Change when the "update" button is pressed...
+### server.R
+server <- shinyServer(function(input, output, session) {
+# Define a reactive expression for the document term matrix
+terms <- reactive({
+ # Change when the "update" button is pressed...    
     input$update
-    # ...but not for anything else
     isolate({
       withProgress({
         setProgress(message = "Processing corpus...")
-        getTermMatrix(input$selection)
+        getTermMatrix(phrases[[input$selection]])
       })
     })
   })
+# Make the wordcloud drawing predictable during a session 
+wordcloud_rep <- repeatable(wordcloud)
 
-  # Make the wordcloud drawing predictable during a session
-  wordcloud_rep <- repeatable(wordcloud)
-
-  output$plot <- renderPlot({
-    v <- terms()
-    wordcloud_rep(names(v), v, scale=c(4,0.5),
-                  min.freq = input$freq, max.words=input$max,
-                  colors=brewer.pal(8, "Dark2"))
-  })
-}
+#Create the wordcloud
+output$plot <- renderPlot({
+v <- terms()
+wordcloud_rep(names(v), v, scale=c(5,0.5),
+              min.freq = input$freq, max.words=input$max,
+              colors=brewer.pal(11, "Spectral"))
+})
+})
