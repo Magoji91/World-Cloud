80b0117e5ab414dd40644f25273d5dcce86c4e9a
Magoji91 <smsanda2004@gmail.com>
2017-04-13 22:55:38 -0300

    Update Global.R

--- Global.R
+++ Global.R
@@ -1,31 +1,18 @@
 library(tm)
 library(wordcloud)
 library(memoise)
+library(shiny)
+library(stringr)
 
-# The list of valid books
-books <<- list("A Mid Summer Night's Dream" = "summer",
-              "The Merchant of Venice" = "merchant",
-              "Romeo and Juliet" = "romeo")
+phrases <- list(Seuss = c("Do","not","cry","because","it","is","over","smile","because","it","happened"),
+                Wilde = c("Be","yourself","everyone","else", "is", "already", "taken”))
 
-# Using "memoise" to automatically cache the results
-getTermMatrix <- memoise(function(book) {
-  # Careful not to let just any name slip in here; a
-  # malicious user could manipulate this value.
-  if (!(book %in% books))
-    stop("Unknown book")
-
-  text <- readLines(sprintf("./%s.txt.gz", book),
-    encoding="UTF-8")
-
+getTermMatrix <- memoise(function(text) {
   myCorpus = Corpus(VectorSource(text))
   myCorpus = tm_map(myCorpus, content_transformer(tolower))
-  myCorpus = tm_map(myCorpus, removePunctuation)
-  myCorpus = tm_map(myCorpus, removeNumbers)
-  myCorpus = tm_map(myCorpus, removeWords,
-         c(stopwords("SMART"), "thy", "thou", "thee", "the", "and", "but"))
-
+  
   myDTM = TermDocumentMatrix(myCorpus,
-              control = list(minWordLength = 1))
+                             control = list(minWordLength = 1))
   
   m = as.matrix(myDTM)
   
